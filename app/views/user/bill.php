d55e753cf02ff3ab62acb75872cc006061241a53
tamtv <tamtvph43776@fpt.edu.vn>
2023-04-04 15:31:21 +0700

    upload file

---
+++ app/views/user/bill.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="thongtin.css">
+</head>
+
+<body>
+    <!-- <div class="container">
+        <div class="style">
+            <h2>Thông tin giao hàng</h2>
+        </div>
+        <div class="content">
+            <div class="content-body">
+                <input type="text" placeholder="Tên người nhận" class="content-item"> <br>
+                <input type="text" placeholder="Số điện thoại người nhận" class="content-item"><br>
+                <input type="text" placeholder="Email người nhận" class="content-item"><br>
+                <input type="text" placeholder="Địa chỉ người nhận" class="content-item">
+            </div>
+        </div>
+
+    </div> -->
+
+    <section>
+        <!--Bắt Đầu Phần Hình Ảnh-->
+        <div class="img-bg">
+            <img src="https://toplist.vn/images/800px/tocotoco-bubble-tea-258910.jpg" alt="Hình Ảnh Minh Họa">
+        </div>
+        <!--Kết Thúc Phần Hình Ảnh-->
+        <!--Bắt Đầu Phần Nội Dung-->
+        <div class="noi-dung">
+            <div class="form">
+                <h2>Xác nhận đặt hàng</h2>
+                <form action="">
+                    <div class="input-form">
+                        <span>Tên Người Dùng</span>
+                        <input type="text" name="">
+                    </div>
+                    <div class="input-form">
+                        <span>Số điện thoại</span>
+                        <input type="password" name="">
+                    </div>
+
+                    <div class="input-form">
+                        <span>Email</span>
+                        <input type="password" name="">
+                    </div>
+
+                    <div class="input-form">
+                        <span>Địa chỉ</span>
+                        <input type="password" name="">
+                    </div>
+                    
+                    <div class="nho-dang-nhap">
+                        <input type="text" placeholder="Voucher 🔽">
+                    </div>
+
+                    <div class="input-form">
+                        <input type="submit" value="Đặt hàng">
+                    </div>
+                    
+                </form>
+                
+            </div>
+        </div>
+        <!--Kết Thúc Phần Nội Dung-->
+    </section>
+</body>
+
+</html>
